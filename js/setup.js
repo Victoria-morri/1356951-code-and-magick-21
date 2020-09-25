425ba21553940c2bfb62b8896c0c0f5b3271e241
Victoria <morriface@mail.ru>
2020-09-25 17:42:46 +0300

    Рефакторинг кода №2

--- js/setup.js
+++ js/setup.js
@@ -13,33 +13,30 @@ const WIZARD_COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146,
 const WIZARD_EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
 const WIZARD_QUANTITY = 4;
 
+const getRandomImport = function (imports) {
+  const randomImport = imports[Math.floor(Math.random() * imports.length)];
+  return randomImport;
+};
+
+const getWizardData = function () {
+  const wizardData = {};
+  wizardData.name = `${getRandomImport(WIZARD_NAMES)} ${getRandomImport(WIZARD_SURNAMES)}`;
+  wizardData.coatColor = getRandomImport(WIZARD_COAT_COLORS);
+  wizardData.eyesColor = getRandomImport(WIZARD_EYES_COLORS);
+  return wizardData;
+};
+
+const createWizard = function (wizard) {
+  const similarWizardForm = TEMPLATE.cloneNode(true);
+  similarWizardForm.querySelector(`.setup-similar-label`).textContent = wizard.name;
+  similarWizardForm.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
+  similarWizardForm.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
+  return similarWizardForm;
+};
 const renderSimilarWizards = function () {
-
-  const wizardPrototype = {};
-
-  const getRandomImport = function (imports) {
-    const randomImport = imports[Math.floor(Math.random() * imports.length)];
-    return randomImport;
-  };
-
-  const getWizardData = function (wizardData) {
-    wizardData.name = `${getRandomImport(WIZARD_NAMES)} ${getRandomImport(WIZARD_SURNAMES)}`;
-    wizardData.coatColor = getRandomImport(WIZARD_COAT_COLORS);
-    wizardData.eyesColor = getRandomImport(WIZARD_EYES_COLORS);
-    return wizardData;
-  };
-
-  const createWizard = function (wizard) {
-    const similarWizardForm = TEMPLATE.cloneNode(true);
-    similarWizardForm.querySelector(`.setup-similar-label`).textContent = wizard.name;
-    similarWizardForm.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
-    similarWizardForm.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
-    return similarWizardForm;
-  };
-
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < WIZARD_QUANTITY; i++) {
-    fragment.appendChild(createWizard(getWizardData(wizardPrototype)));
+    fragment.appendChild(createWizard(getWizardData()));
   }
   return fragment;
 };
