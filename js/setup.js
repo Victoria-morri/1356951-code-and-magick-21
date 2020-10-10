44527a042ac128b8c66f5328115aa82f1d5cbcc5
Victoria <morriface@mail.ru>
2020-10-10 14:50:59 +0300

    задание выполненно частично

--- js/setup.js
+++ js/setup.js
@@ -1,12 +1,5 @@
 'use strict';
 
-const WIZARDS_POPUP_ELEMENT = document.querySelector(`.setup`);
-WIZARDS_POPUP_ELEMENT.classList.remove(`hidden`);
-WIZARDS_POPUP_ELEMENT.querySelector(`.setup-similar`).classList.remove(`hidden`);
-const WIZARDS_LIST_ELEMENT = WIZARDS_POPUP_ELEMENT.querySelector(`.setup-similar-list`);
-
-const TEMPLATE = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
-
 const WIZARD_NAMES = [`Иван`, `Хуан`, `Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
 const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const WIZARD_COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
@@ -27,7 +20,7 @@ const getWizardData = function () {
 };
 
 const createWizard = function (wizard) {
-  const similarWizardForm = TEMPLATE.cloneNode(true);
+  const similarWizardForm = template.cloneNode(true);
   similarWizardForm.querySelector(`.setup-similar-label`).textContent = wizard.name;
   similarWizardForm.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
   similarWizardForm.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
@@ -40,4 +33,69 @@ const renderSimilarWizards = function () {
   }
   return fragment;
 };
-WIZARDS_LIST_ELEMENT.appendChild(renderSimilarWizards());
+
+const onPopupEscPress = function (evt) {
+  if (evt.key === `Escape`) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+const closePopup = function () {
+  wizadrPopupElement.classList.add(`hidden`);
+  wizadrPopupElement.querySelector(`.setup-similar`).classList.add(`hidden`);
+  document.removeEventListener(`keydown`, onPopupEscPress);
+};
+
+const openPopup = function () {
+  wizadrPopupElement.classList.remove(`hidden`);
+  wizadrPopupElement.querySelector(`.setup-similar`).classList.remove(`hidden`);
+  document.addEventListener(`keydown`, onPopupEscPress);
+};
+
+const wizadrPopupElement = document.querySelector(`.setup`);
+const popupOpenElement = document.querySelector(`.setup-open`);
+const popupCloseElement = document.querySelector(`.setup-close`);
+
+const wizardFormElement = wizadrPopupElement.querySelector(`.setup-wizard-form`);
+// wizadrPopupElement.classList.remove(`hidden`);
+// wizadrPopupElement.querySelector(`.setup-similar`).classList.remove(`hidden`);
+const wizardListElement = wizadrPopupElement.querySelector(`.setup-similar-list`);
+const submitElement = wizadrPopupElement.querySelector(`.setup-submit`);
+const template = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
+// const wizardCoatElement = wizardFormElement.querySelector(`.setup-wizard`).content.querySelector(`.wizard-coat`);
+
+popupOpenElement.addEventListener(`click`, function () {
+  openPopup();
+});
+
+popupOpenElement.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    openPopup();
+  }
+});
+
+popupCloseElement.addEventListener(`click`, function () {
+  closePopup();
+});
+
+popupCloseElement.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    closePopup();
+  }
+});
+
+submitElement.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    wizardFormElement.submit();
+  }
+});
+submitElement.addEventListener(`click`, function () {
+  wizardFormElement.submit();
+});
+
+/* wizardCoatElement.addEventListener(`click`, function () {
+  wizardCoatElement.style.fill = `rgb (215, 210, 55)`;
+});
+*/
+wizardListElement.appendChild(renderSimilarWizards());
